24f451f9658879fd70120e0a58797373ebb2a5bd
ShahadAlameel <shahad.a@outlook.com>
2025-04-12 16:41:52 +0300

    add 'sendfile' functionality

--- chat_app_connector/chat_app.py
+++ chat_app_connector/chat_app.py
@@ -6,20 +6,21 @@
 # This code contains trade secrets of Real-Time Innovations, Inc.
 ############################################################################
 
-import os 
-import argparse 
+import os
+import argparse
 import threading
-from posixpath import split 
+from posixpath import split
 from time import sleep
 from os import error, path as os_path
+import base64
 
 file_path = os_path.dirname(os_path.realpath(__file__))
 parser = argparse.ArgumentParser(description='DDS Chat Application')
 
-parser.add_argument('user', help='User Name', type=str) 
+parser.add_argument('user', help='User Name', type=str)
 parser.add_argument('group', help='Group Name', type=str)
-parser.add_argument('-f', '--firstname', help='First Name', type=str, default='') 
-parser.add_argument('-1', '--lastname', help='Last Name', type=str, default='')
+parser.add_argument('-f', '--firstname', help='First Name', type=str, default='')
+parser.add_argument('-l', '--lastname', help='Last Name', type=str, default='')
 
 args = parser.parse_args()
 
@@ -33,15 +34,15 @@ finish_thread = False
 offline_message_queue = {}
 
 # Queue the messages for offline users
-def queue_message_for_user(from_user, to_user, message):
+def queue_message_for_user(from_user, to_user, message, file_data=None, file_name=None):
     global offline_message_queue
 
     with lock:
         # If the user is offline, queue the message
         if to_user not in offline_message_queue:
             offline_message_queue[to_user] = []
-        
-        offline_message_queue[to_user].append((from_user, message))
+
+        offline_message_queue[to_user].append((from_user, message, file_data, file_name))
     print(f"Message queued for {to_user} from:{from_user}. Message: '{message}'")
 
 # Deliver queued messages to the user when they come online
@@ -51,16 +52,22 @@ def deliver_queued_messages(user):
     with lock:
         if user in offline_message_queue:
             messages = offline_message_queue[user]
-            for from_user, message in messages:
+            for from_user, message, file_data, file_name in messages:
                 print(f"Delivering message to {user} from {from_user}: {message}")
 
                 # Send the queued message via DDS
-                message_output.instance.set_string("fromUser",from_user)  
-                message_output.instance.set_string("toUser", user)  
+                message_output.instance.set_string("fromUser", from_user)
+                message_output.instance.set_string("toUser", user)
                 message_output.instance.set_string("toGroup", "")  # Empty group if not needed
-                message_output.instance.set_string("message", message)  
+                message_output.instance.set_string("message", message)
+                if file_data and file_name:
+                    message_output.instance.set_string("fileData", file_data)
+                    message_output.instance.set_string("fileName", file_name)
+                else:
+                    message_output.instance.set_string("fileData", "")
+                    message_output.instance.set_string("fileName", "")
 
-                message_output.write() 
+                message_output.write()
                 sleep(3)
 
             # After delivering messages, clear the queue for the user
@@ -78,7 +85,7 @@ def user_subscriber_task(user_input):
         with lock:
             user_input.read()
             for sample in user_input.samples:
-                if(sample.info['sample_state'] == "NOT_READ") and (sample.valid_data == False) and (sample.info['instance_state'] == "NOT_ALIVE_NO_WRITERS"):
+                if (sample.info['sample_state'] == "NOT_READ") and (sample.valid_data == False) and (sample.info['instance_state'] == "NOT_ALIVE_NO_WRITERS"):
                     data = sample.get_dictionary()
                     print("#Dropped user: " + data['username'])
                     offline_message_queue[data['username']] = []  # Initialize the queue for offline user
@@ -103,10 +110,20 @@ def message_subscriber_task(message_input):
             message_input.take()
             for sample in message_input.samples.valid_data_iter:
                 data = sample.get_dictionary()
-                print("#New chat message from user: "+ data['fromUser'] + ". Message: '" + data['message'] + "'")
+                print("#New chat message from user: " + data['fromUser'] + ". Message: '" + data['message'] + "'")
+                if data['fileName'] and data['fileData']:
+                    print(f"#File received: {data['fileName']}")
+                    # You can save the file here or display it
+                    try:
+                        file_data = base64.b64decode(data['fileData'])
+                        with open(data['fileName'], 'wb') as f:
+                            f.write(file_data)
+                        print(f"#File saved as: {data['fileName']}")
+                    except Exception as e:
+                        print(f"Error saving file: {e}")
 
 def command_task(user, message_output, user_input):
-    global finish_thread  
+    global finish_thread
 
     while finish_thread == False:
         command = input("Please enter command \n")
@@ -122,7 +139,7 @@ def command_task(user, message_output, user_input):
                     # detecting alive instances
                     if sample.info['instance_state'] == 'ALIVE':
                         data = sample.get_dictionary()
-                        print("#username/group: " + data['username']+ "/" + data['group'])
+                        print("#username/group: " + data['username'] + "/" + data['group'])
 
         elif command.startswith("send "):
             destination = command.split(maxsplit=2)
@@ -132,6 +149,8 @@ def command_task(user, message_output, user_input):
                     message_output.instance.set_string("toUser", destination[1])
                     message_output.instance.set_string("toGroup", destination[1])
                     message_output.instance.set_string("message", destination[2])
+                    message_output.instance.set_string("fileData", "")
+                    message_output.instance.set_string("fileName", "")
 
                     # Before sending, check if the user is offline
                     if destination[1] in offline_message_queue:
@@ -139,21 +158,49 @@ def command_task(user, message_output, user_input):
                     else:
                         message_output.write()  # Send the message if the user is online
 
-                    #message_output.write()
+            else:
+                print("Wrong usage. Use \"send user|group message\"\n")
+
+        elif command.startswith("sendfile "):
+            destination_file = command.split(maxsplit=2)
+            if len(destination_file) == 3:
+                destination = destination_file[1]
+                file_path_send = destination_file[2]
+                try:
+                    with open(file_path_send, 'rb') as f:
+                        file_data = base64.b64encode(f.read()).decode('utf-8')
+                    file_name = os.path.basename(file_path_send)
+
+                    with lock:
+                        message_output.instance.set_string("fromUser", user)
+                        message_output.instance.set_string("toUser", destination)
+                        message_output.instance.set_string("toGroup", destination)
+                        message_output.instance.set_string("message", f"File: {file_name}")
+                        message_output.instance.set_string("fileData", file_data)
+                        message_output.instance.set_string("fileName", file_name)
+
+                        if destination in offline_message_queue:
+                            queue_message_for_user(user, destination, f"File: {file_name}", file_data, file_name)
+                        else:
+                            message_output.write()
+
+                except FileNotFoundError:
+                    print(f"File not found: {file_path_send}")
+                except Exception as e:
+                    print(f"Error sending file: {e}")
 
             else:
-                print("Wrong usage. Use \"senf user|group message\"\n")
+                print("Wrong usage. Use \"sendfile user|group filepath\"\n")
 
         else:
             print("Unknown command\n")
 
 with rti.open_connector(
     config_name="Chat_ParticipantLibrary::ChatParticipant",
-    url= file_path + "/chat_app.xml") as connector:
+    url=file_path + "/chat_app.xml") as connector:
 
     user_output = connector.get_output("ChatUserPublisher::ChatUser_Writer")
     message_output = connector.get_output("ChatMessagePublisher::ChatMessage_Writer")
-
 
     user_input = connector.get_input("ChatUserSubscription::ChatUser_Reader")
     message_input = connector.get_input("ChatMessageSubscription::ChatMessage_Reader")
@@ -162,10 +209,10 @@ with rti.open_connector(
     user_output.instance.set_string("group", args.group)
 
     if args.firstname != "":
-        user_output.instance.set_string("firstName", args.firstName)
+        user_output.instance.set_string("firstName", args.firstname)
     if args.lastname != "":
-        user_output.instance.set_string("lastName", args.lastName)
-        
+        user_output.instance.set_string("lastName", args.lastname)
+
     user_output.write()
 
     t1 = threading.Thread(target=command_task, args=(args.user, message_output, user_input,))
